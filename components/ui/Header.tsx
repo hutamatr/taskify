8d462e8144eab4a242f60b7cbf41cf6093721a59
hutamatr <hutamatr@gmail.com>
2023-06-17 04:56:18 +0700

    feat: create header base component

---
+++ components/ui/Header.tsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet, ViewProps, StyleProp, ViewStyle } from 'react-native';
+import { useTheme } from 'react-native-paper';
+
+interface IHeaderProps extends ViewProps {
+  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+}
+
+export default function Header({ children, style, ...props }: IHeaderProps) {
+  const theme = useTheme();
+
+  return (
+    <View
+      style={[styles.headerContainer, { backgroundColor: theme.colors.inversePrimary }, style]}
+      {...props}
+    >
+      {children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    paddingHorizontal: 16,
+    paddingBottom: 24,
+    paddingTop: 150,
+  },
+});
