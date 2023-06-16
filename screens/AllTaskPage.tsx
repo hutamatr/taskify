42f61199f81a20883dbe143895d56fb8078d24a0
hutamatr <hutamatr@gmail.com>
2023-06-17 04:51:52 +0700

    feat: add tasks header, filter, list components

--- screens/AllTaskPage.tsx
+++ screens/AllTaskPage.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { AnimatedFAB } from 'react-native-paper';
+
+import TasksHeader from '../components/tasks-page/TasksHeader';
+import TaskFilter from '../components/tasks-page/TasksFilter';
+import TaskList from '../components/tasks-page/TasksList';
+import useHandleScroll from '../hooks/useHandleScroll';
 
 export default function AllTaskPage() {
+  const { handleScroll, showButton } = useHandleScroll();
+
   return (
-    <View>
-      <Text>AllTaskPage</Text>
+    <View style={styles.container}>
+      <TasksHeader />
+      <TaskFilter />
+      <TaskList onScroll={handleScroll} />
+      <AnimatedFAB
+        icon="plus"
+        variant="tertiary"
+        label="Add Task"
+        extended={showButton}
+        onPress={() => console.log('Pressed')}
+        visible={true}
+        animateFrom="right"
+        iconMode="dynamic"
+        style={styles.fab}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  },
+});
