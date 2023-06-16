ce58b4d59c76541b596e47bc9d30cb108998543e
hutamatr <hutamatr@gmail.com>
2023-06-17 04:41:43 +0700

    feat: create initial store with zustand

---
+++ store/tabsBarSlice.ts
@@ -0,0 +1,11 @@
+import { StateCreator } from 'zustand';
+
+export interface ITabsBar {
+  isTabsBarShow: boolean;
+  onTabsBarShowHandler: (isExtended: boolean) => void;
+}
+
+export const tabsBarSlice: StateCreator<ITabsBar, [], [], ITabsBar> = (set) => ({
+  isTabsBarShow: true,
+  onTabsBarShowHandler: (isExtended) => set({ isTabsBarShow: isExtended }),
+});
