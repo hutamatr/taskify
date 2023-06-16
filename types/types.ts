6c1258feb8a91416a84373231b1d0f82e0764477
hutamatr <hutamatr@gmail.com>
2023-06-17 04:40:46 +0700

    refactor: refactor task types & root-stack-params-list

--- types/types.ts
+++ types/types.ts
@@ -1,29 +1,38 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { MaterialBottomTabNavigationProp } from '@react-navigation/material-bottom-tabs';
 
 export interface ITask {
   id?: string;
   author: string;
   description: string;
   isCompleted: boolean;
-  createdAt?: string;
-  updatedAt?: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ICategories {
+  id?: string;
+  name: string;
 }
 
 export type RootStackParamList = {
-  Category: undefined;
+  Home: undefined;
+  Categories: undefined;
   Account: undefined;
   Tasks: undefined;
+  CreateTask: undefined;
+  HomeTabs: undefined;
 };
 
-export type RootStackParamListTabs = {
-  Home: undefined;
-  CategoryStack: undefined;
-  AccountStack: undefined;
-  AllTasksStack: undefined;
-};
+// export type RootStackParamListTabs = {
+//   Home: undefined;
+//   CategoryStack: undefined;
+//   AccountStack: undefined;
+//   AllTasksStack: undefined;
+// };
 
-export type HomeNavigationProp = NativeStackNavigationProp<RootStackParamListTabs, 'Home'>;
+export type HomeNavigationProp = MaterialBottomTabNavigationProp<RootStackParamList, 'Home'>;
 
-export type CategoryNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Category'>;
+export type CategoryNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Categories'>;
 
 export type AccountNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Account'>;
