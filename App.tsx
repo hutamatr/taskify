28ac83a95fa079a55fcdd09ddd04ef36abfe5e1c
hutamatr <hutamatr@gmail.com>
2023-06-20 02:05:48 +0700

    style: add new import

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'expo-dev-client';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider, MD3LightTheme as DefaultTheme } from 'react-native-paper';
@@ -8,9 +9,12 @@ import {
   PlusJakartaSans_700Bold,
   PlusJakartaSans_600SemiBold,
 } from '@expo-google-fonts/plus-jakarta-sans';
+import { StatusBar } from 'expo-status-bar';
 
 import CreateTaskPage from './screens/CreateTaskPage';
+import CreateCategoriesPage from './screens/CreateCategoriesPage';
 import HomeTabsNavigation from './navigation/HomeTabsNavigation';
+import Text from './components/ui/Text';
 
 import { RootStackParamList } from './types/types';
 
@@ -39,24 +43,58 @@ export default function App() {
     return null;
   }
   return (
-    <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="HomeTabs"
-            component={HomeTabsNavigation}
-            options={{
-              title: '',
-              headerShadowVisible: false,
-              headerStyle: { backgroundColor: theme.colors.inversePrimary },
-              headerShown: false,
-              statusBarStyle: 'dark',
-              statusBarColor: theme.colors.inversePrimary,
-            }}
-          />
-          <Stack.Screen name="CreateTask" component={CreateTaskPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <>
+      <StatusBar style="auto" animated hideTransitionAnimation="fade" />
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeTabs"
+              component={HomeTabsNavigation}
+              options={{
+                title: '',
+                headerShadowVisible: false,
+                headerStyle: { backgroundColor: theme.colors.inversePrimary },
+                headerShown: false,
+                statusBarStyle: 'dark',
+                statusBarColor: theme.colors.inversePrimary,
+              }}
+            />
+            <Stack.Screen
+              name="CreateTask"
+              component={CreateTaskPage}
+              options={{
+                title: 'Create Task',
+                headerShadowVisible: false,
+                headerTitle: () => (
+                  <Text fontType="regular" variant="headlineSmall">
+                    Create Task
+                  </Text>
+                ),
+                headerStyle: { backgroundColor: theme.colors.inversePrimary },
+                statusBarStyle: 'dark',
+                statusBarColor: theme.colors.inversePrimary,
+              }}
+            />
+            <Stack.Screen
+              name="CreateCategories"
+              component={CreateCategoriesPage}
+              options={{
+                title: 'Create Categories',
+                headerShadowVisible: false,
+                headerTitle: () => (
+                  <Text fontType="regular" variant="headlineSmall">
+                    Create Categories
+                  </Text>
+                ),
+                headerStyle: { backgroundColor: theme.colors.inversePrimary },
+                statusBarStyle: 'dark',
+                statusBarColor: theme.colors.inversePrimary,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </>
   );
 }
