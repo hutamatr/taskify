0d7e312d330eb2e11d40abf819ea5982e3c4a78c
hutamatr <hutamatr@gmail.com>
2023-06-21 03:58:57 +0700

    feat: create profile data list

---
+++ components/profile-page/ProfileList.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+import Text from '../ui/Text';
+
+export default function ProfileList() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <Text fontType="semibold" variant="titleLarge">
+          Name
+        </Text>
+        <Text fontType="medium" variant="bodyLarge">
+          Hutama Trirahmanto
+        </Text>
+      </View>
+      <View style={styles.profileContainer}>
+        <Text fontType="semibold" variant="titleLarge">
+          Email
+        </Text>
+        <Text fontType="medium" variant="bodyLarge">
+          hutamatr@gmail.com
+        </Text>
+      </View>
+      <Button mode="outlined" style={styles.button}>
+        <Text fontType="semibold" variant="titleMedium">
+          Sign Out
+        </Text>
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 24,
+    marginHorizontal: 16,
+    marginVertical: 24,
+  },
+  profileContainer: {
+    rowGap: 8,
+  },
+  button: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+});
