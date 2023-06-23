194c08f5f253ff0426c603ee6293ae4db995618b
hutamatr <hutamatr@gmail.com>
2023-06-23 20:23:56 +0700

    feat: add zustand storage persist

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,7 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 import { type ITaskSlice, tasksSlice } from './tasksSlice';
 
-export const useStore = create<ITaskSlice>((...a) => ({
-  ...tasksSlice(...a),
-}));
+export const useStore = create<ITaskSlice>()(
+  persist(
+    (...a) => ({
+      ...tasksSlice(...a),
+    }),
+    {
+      name: 'tasks-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
