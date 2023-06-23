11890b41c0500752f2822f41426cfc0308b7f17f
hutamatr <hutamatr@gmail.com>
2023-06-23 20:26:57 +0700

    feat: add add,update,delete handler in task slice

--- store/tasksSlice.ts
+++ store/tasksSlice.ts
@@ -1,6 +1,14 @@
 import { type StateCreator } from 'zustand';
 
-import { getAllTasks, getTask } from '../api/api';
+import {
+  addTask,
+  deleteTask,
+  getAllCompleted,
+  getAllInProgress,
+  getAllTasks,
+  getTask,
+  updateTask,
+} from '../api/api';
 import { type ITask } from '../types/types';
 
 interface ITaskError {
@@ -9,47 +17,93 @@ interface ITaskError {
 }
 export interface ITaskSlice {
   tasks: ITask[];
+  // recentTasks: ITask[];
+  inProgressTasks: ITask[];
+  completedTasks: ITask[];
   task: ITask | object;
   isLoading: boolean;
   error: ITaskError;
   fetchAllTasksHandler: () => void;
-  fetchTaskHandler?: (taskId: string) => void;
-  addTaskHandler?: () => void;
-  updateTaskHandler?: () => void;
-  deleteTaskHandler?: () => void;
+  fetchAllInProgress: () => void;
+  fetchAllCompleted: () => void;
+  fetchTaskHandler: (taskId: string) => void;
+  addTaskHandler: (task: ITask) => void;
+  updateTaskHandler: (task: ITask) => void;
+  deleteTaskHandler: (taskId: string) => void;
 }
 
 export const tasksSlice: StateCreator<ITaskSlice, [], [], ITaskSlice> = (set) => ({
   tasks: [],
+  // recentTasks: [],
+  inProgressTasks: [],
+  completedTasks: [],
   task: {},
   isLoading: false,
   error: { isError: false, errorMessage: '' },
   fetchAllTasksHandler: () => {
     try {
       set({ isLoading: true });
-      const tasksData = getAllTasks();
-      set(
-        () => ({
-          isLoading: false,
-          tasks: tasksData,
-        }),
-        true
-      );
+      getAllTasks(set);
     } catch (error) {
       set({ isLoading: false, error: { isError: true, errorMessage: 'Failed get all task data' } });
+    }
+  },
+  fetchAllInProgress: () => {
+    try {
+      set({ isLoading: true });
+      getAllInProgress(set);
+    } catch (error) {
+      set({ isLoading: false, error: { isError: true, errorMessage: 'Failed get all task data' } });
+    }
+  },
+  fetchAllCompleted: () => {
+    try {
+      set({ isLoading: true });
+      getAllCompleted(set);
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: { isError: true, errorMessage: 'Failed get all task data' },
+      });
     }
   },
   fetchTaskHandler: (taskId) => {
     try {
       set({ isLoading: true });
       const taskData = getTask(taskId);
-
-      set({
+      set(() => ({
         isLoading: false,
         task: taskData,
-      });
+      }));
     } catch (error) {
       set({ isLoading: false, error: { isError: true, errorMessage: 'Failed get task data' } });
+    }
+  },
+  addTaskHandler: async ({ title, description, date }: ITask) => {
+    try {
+      set({ isLoading: true });
+      await addTask({ title, description, date });
+      set({ isLoading: false });
+    } catch (error) {
+      set({ isLoading: false, error: { isError: true, errorMessage: 'Failed send task' } });
+    }
+  },
+  updateTaskHandler: async (task) => {
+    try {
+      set({ isLoading: true });
+      await updateTask(task);
+      set({ isLoading: false });
+    } catch (error) {
+      set({ isLoading: false, error: { isError: true, errorMessage: 'Failed update task' } });
+    }
+  },
+  deleteTaskHandler: async (taskId) => {
+    try {
+      set({ isLoading: true });
+      await deleteTask(taskId);
+      set({ isLoading: false });
+    } catch (error) {
+      set({ isLoading: false, error: { isError: true, errorMessage: 'Failed delete task' } });
     }
   },
 });
