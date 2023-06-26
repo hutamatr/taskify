c328864f80e66d4c306becc48f27943ce1c6319b
hutamatr <hutamatr@gmail.com>
2023-06-26 23:42:38 +0700

    feat: create  categories slice

---
+++ store/categoriesSlice.ts
@@ -0,0 +1,70 @@
+import type { StateCreator } from 'zustand';
+
+import {
+  addCategories,
+  deleteCategory,
+  deleteCategoryWithTasks,
+  getAllCategories,
+} from '../api/api';
+import type { ICategories } from '../types/types';
+
+export interface ICategoriesSlice {
+  categories: ICategories[];
+  isLoading: boolean;
+  error: { isError: boolean; errorMessage: string };
+  fetchAllCategoriesHandler: () => void;
+  addCategoryHandler: (category: ICategories) => void;
+  deleteCategoryHandler: (categoryId: string, isDeleteWithTasks: boolean) => void;
+}
+
+export const categoriesSlice: StateCreator<ICategoriesSlice, [], [], ICategoriesSlice> = (set) => ({
+  categories: [],
+  isLoading: false,
+  error: { isError: false, errorMessage: '' },
+  fetchAllCategoriesHandler: () => {
+    try {
+      set({
+        isLoading: true,
+        error: { isError: false, errorMessage: '' },
+      });
+      getAllCategories(set);
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: { isError: true, errorMessage: 'Failed get all categories data' },
+      });
+    }
+  },
+  addCategoryHandler: async (category) => {
+    try {
+      set({
+        isLoading: true,
+        error: { isError: false, errorMessage: '' },
+      });
+      await addCategories(category);
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: { isError: true, errorMessage: 'Failed get all categories data' },
+      });
+    }
+  },
+  deleteCategoryHandler: async (categoryId, isDeleteWithTasks) => {
+    try {
+      set({
+        isLoading: true,
+        error: { isError: false, errorMessage: '' },
+      });
+      if (isDeleteWithTasks) {
+        await deleteCategoryWithTasks(categoryId);
+      } else {
+        await deleteCategory(categoryId);
+      }
+    } catch (error) {
+      set({
+        isLoading: false,
+        error: { isError: true, errorMessage: 'Failed get all categories data' },
+      });
+    }
+  },
+});
