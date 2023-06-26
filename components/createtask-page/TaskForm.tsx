d515da3fc36a49dd1dd27160ec06599a67448bdc
hutamatr <hutamatr@gmail.com>
2023-06-26 23:50:37 +0700

    feat: add category chooser in task form

--- components/createtask-page/TaskForm.tsx
+++ components/createtask-page/TaskForm.tsx
@@ -1,43 +1,78 @@
 import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Dimensions, FlatList, StyleSheet, View } from 'react-native';
 import DatePicker from 'react-native-date-picker';
 import { Button, TextInput } from 'react-native-paper';
+import { shallow } from 'zustand/shallow';
 
+import CategoriesItem from '../categories-page/CategoriesItem';
+import Loading from '../ui/Loading';
 import Text from '../ui/Text';
-import { addTask } from '../../api/api';
 import useInputState from '../../hooks/useInputState';
-import { CreateTaskNavigationProp } from '../../types/types';
+import { useStore } from '../../store/useStore';
+import { CreateTaskNavigationProp, ICategories, ITask } from '../../types/types';
+import { formatData } from '../../utils/formatDataList';
+
+const numColumns = 2;
 
 export default function TaskForm() {
+  const [open, setOpen] = useState(false);
   const { input, onChangeInputHandler, setInput } = useInputState<{
     title: string;
     description: string;
     date: Date;
-  }>({ inputState: { title: '', description: '', date: new Date() } });
-  const [open, setOpen] = useState(false);
+    categoryId: string;
+    categoryName: string;
+  }>({
+    inputState: { title: '', description: '', date: new Date(), categoryId: '', categoryName: '' },
+  });
 
   const navigation = useNavigation<CreateTaskNavigationProp>();
 
+  const { categories, addTask, fetchAllCategories, isLoading, error } = useStore(
+    (state) => ({
+      addTask: state.addTaskHandler,
+      fetchAllCategories: state.fetchAllCategoriesHandler,
+      categories: state.categories,
+      isLoading: state.isLoading,
+      error: state.error,
+    }),
+    shallow
+  );
+
+  useEffect(() => {
+    fetchAllCategories();
+  }, []);
+
+  const pickedCategoriesHandler = ({ name, id }: ICategories) => {
+    setInput((prevState) => ({ ...prevState, categoryId: id as string, categoryName: name }));
+  };
+
+  const addNewCategoryHandler = () => {
+    navigation.navigate('Categories');
+  };
+
   const submitTaskHandler = async () => {
-    if (!input.title || !input.description) {
+    if (!input.title || !input.description || !input.date || !input.categoryId) {
       return;
     }
 
-    const newTask = {
+    const newTask: ITask = {
       title: input.title,
       description: input.description,
       date: input.date.toISOString(),
+      categoryId: input.categoryId,
+      categoryName: input.categoryName,
     };
 
     try {
-      await addTask(newTask);
+      addTask(newTask);
       navigation.goBack();
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error);
     } finally {
-      setInput({ title: '', description: '', date: new Date() });
+      setInput({ title: '', description: '', date: new Date(), categoryId: '', categoryName: '' });
     }
   };
 
@@ -77,6 +112,63 @@ export default function TaskForm() {
           setOpen(false);
         }}
       />
+      <View style={styles.categoriesContainer}>
+        <View style={styles.categoriesTitleContainer}>
+          <Text fontType="medium" variant="titleLarge">
+            Pick Category
+          </Text>
+          {categories.length > 0 && (
+            <Button icon="plus" onPress={addNewCategoryHandler}>
+              Add Category
+            </Button>
+          )}
+        </View>
+        {isLoading && <Loading size="large" />}
+        {error?.isError && (
+          <Text fontType="medium" style={styles.error} variant="headlineSmall">
+            {error.errorMessage}
+          </Text>
+        )}
+        {!isLoading && !error.isError && categories.length === 0 && (
+          <View style={styles.categoriesEmptyContainer}>
+            <Text fontType="medium" variant="headlineSmall">
+              Categories Empty
+            </Text>
+            <Button icon="plus" onPress={addNewCategoryHandler} mode="elevated">
+              Add Category
+            </Button>
+          </View>
+        )}
+        {!isLoading && !error.isError && categories.length > 0 && (
+          <FlatList
+            data={formatData(categories, numColumns)}
+            renderItem={({ item }) => {
+              if (!item.id) {
+                return (
+                  <CategoriesItem
+                    style={[styles.categoriesItem, { backgroundColor: 'transparent' }]}
+                    mode={undefined}
+                    name=""
+                  />
+                );
+              }
+              return (
+                <CategoriesItem
+                  {...item}
+                  mode={item.id === input.categoryId ? 'contained' : 'outlined'}
+                  style={styles.categoriesItem}
+                  textVariant="bodyLarge"
+                  onPress={pickedCategoriesHandler.bind(null, item)}
+                />
+              );
+            }}
+            keyExtractor={({ id }) => id as string}
+            contentContainerStyle={styles.container}
+            columnWrapperStyle={styles.row}
+            numColumns={numColumns}
+          />
+        )}
+      </View>
       <Button mode="contained" style={styles.button} onPress={submitTaskHandler}>
         <Text fontType="semibold" variant="titleMedium">
           Add Task
@@ -87,9 +179,43 @@ export default function TaskForm() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+  categoriesContainer: {
+    flex: 1,
+    marginBottom: 64,
+  },
+  categoriesTitleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 8,
+    marginBottom: 16,
+  },
+  categoriesEmptyContainer: {
+    margin: 34,
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: '#cfcf',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    margin: 4,
+  },
   button: {
     position: 'absolute',
     bottom: 0,
     width: '100%',
+  },
+  categoriesItem: {
+    marginHorizontal: 4,
+    height: Dimensions.get('window').width / 6,
+  },
+  error: {
+    textAlign: 'center',
+    margin: 24,
+    color: 'red',
   },
 });
