a97bbdd744523f6ce673f12dba92e40dbf63b979
hutamatr <hutamatr@gmail.com>
2023-07-05 10:08:14 +0700

    refactor: change categories data fetching approach

--- components/categories-page/CategoriesList.tsx
+++ components/categories-page/CategoriesList.tsx
@@ -7,27 +7,28 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import { shallow } from 'zustand/shallow';
 
 import CategoriesItem from './CategoriesItem';
 import Loading from '../ui/Loading';
 import Text from '../ui/Text';
-import { useStore } from '../../store/useStore';
 import type { CategoriesNavigationProp, ICategories } from '../../types/types';
 import { formatData } from '../../utils/formatDataList';
 
 interface ICategoriesListProps {
   onScroll?: (event: NativeSyntheticEvent<NativeScrollEvent>) => void;
+  isLoading: boolean;
+  error: Error | undefined;
+  categories: ICategories[];
 }
 
 const numColumns = 2;
 
-export default function CategoriesList({ onScroll }: ICategoriesListProps) {
-  const { categories, isLoading, error } = useStore(
-    (state) => ({ categories: state.categories, isLoading: state.isLoading, error: state.error }),
-    shallow
-  );
-
+export default function CategoriesList({
+  onScroll,
+  categories,
+  isLoading,
+  error,
+}: ICategoriesListProps) {
   const navigation = useNavigation<CategoriesNavigationProp>();
 
   const viewCategoriesDetailHandler = (category: ICategories) => {
@@ -37,23 +38,23 @@ export default function CategoriesList({ onScroll }: ICategoriesListProps) {
   return (
     <View style={styles.listContainer}>
       {isLoading && <Loading size="large" />}
-      {error?.isError && (
+      {error && (
         <Text fontType="medium" style={styles.error} variant="headlineSmall">
-          {error.errorMessage}
+          {error.message}
         </Text>
       )}
-      {!isLoading && !error.isError && categories.length === 0 && (
+      {!isLoading && !error && categories?.length === 0 && (
         <View style={styles.categoriesEmptyContainer}>
           <Text fontType="medium" variant="headlineSmall">
             Categories Empty
           </Text>
         </View>
       )}
-      {!isLoading && !error.isError && categories.length > 0 && (
+      {!isLoading && !error && categories && categories.length > 0 && (
         <FlatList
           data={formatData(categories, numColumns)}
           renderItem={({ item }) => {
-            if (!item.id) {
+            if (!item.name) {
               return (
                 <CategoriesItem
                   style={[styles.categoriesItem, { backgroundColor: 'transparent' }]}
