0e272449ecf7cb4eef396370b7c3321908bb47ba
hutamatr <hutamatr@gmail.com>
2023-07-05 10:01:45 +0700

    refactor: change task data fetching approach

--- components/home-page/tasks/Tasks.tsx
+++ components/home-page/tasks/Tasks.tsx
@@ -1,15 +1,22 @@
 import { useNavigation } from '@react-navigation/native';
+import { useCollection } from '@skillnation/react-native-firebase-hooks/firestore';
 import { StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import RecentTasks from './RecentTasks';
 import Text from '../../ui/Text';
+import { queryTasks } from '../../../api/api';
+import useFormatData from '../../../hooks/useFormatData';
 import { useStore } from '../../../store/useStore';
-import type { HomeNavigationProp } from '../../../types/types';
+import type { HomeNavigationProp, ITask } from '../../../types/types';
 
 export default function Tasks() {
   const navigation = useNavigation<HomeNavigationProp>();
-  const tasks = useStore((state) => state.tasks);
+  const userInfo = useStore((state) => state.userInfo);
+
+  const [tasks, loading, error] = useCollection(queryTasks(userInfo?.uid as string));
+
+  const tasksData = useFormatData<ITask[]>(tasks);
 
   const showAllTaskHandler = () => {
     navigation.navigate('Tasks');
@@ -22,8 +29,8 @@ export default function Tasks() {
           Recent Task
         </Text>
       </View>
-      <RecentTasks />
-      {tasks.length > 0 && (
+      <RecentTasks tasks={tasksData} isLoading={loading} error={error} />
+      {tasksData.length > 0 && (
         <View style={styles.buttonContainer}>
           <Button
             mode="text"
