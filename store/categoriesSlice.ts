6472db86ab79c9dedcefcba3fd54cf1ed592d490
hutamatr <hutamatr@gmail.com>
2023-07-07 14:05:50 +0700

    refactor: change slice loading & error state to categories status

--- store/categoriesSlice.ts
+++ store/categoriesSlice.ts
@@ -4,37 +4,70 @@ import { addCategories, deleteCategory, deleteCategoryWithTasks } from '../api/a
 import type { ICategories } from '../types/types';
 
 export interface ICategoriesSlice {
-  categoriesLoading: boolean;
-  categoriesError: Error | undefined;
+  categories: ICategories[] | [];
+  categoriesStatus: 'idle' | 'pending' | 'successful' | 'rejected';
+  categoriesError: { error: Error | undefined; errorMessage: string };
+  getAllCategoriesHandler: (
+    categories: ICategories[],
+    loading: boolean,
+    error: Error | undefined
+  ) => void;
   addCategoryHandler: (category: ICategories) => void;
   deleteCategoryHandler: (categoryId: string, isDeleteWithTasks: boolean, userId: string) => void;
 }
 
 export const categoriesSlice: StateCreator<ICategoriesSlice, [], [], ICategoriesSlice> = (set) => ({
-  categoriesLoading: false,
-  categoriesError: undefined,
+  categories: [],
+  categoriesStatus: 'idle',
+  categoriesError: { error: undefined, errorMessage: '' },
+  getAllCategoriesHandler: (categories, loading, error) => {
+    set(
+      (state) => ({
+        ...state,
+        categories: categories,
+        categoriesStatus: loading === true ? 'pending' : 'idle',
+        categoriesError: { error: error, errorMessage: '' },
+      }),
+      true
+    );
+  },
   addCategoryHandler: async (category) => {
     try {
       set({
-        categoriesLoading: true,
+        categoriesStatus: 'pending',
         categoriesError: undefined,
       });
       await addCategories(category);
-    } catch (error) {
-      if (error instanceof Error) {
+      set({ categoriesStatus: 'successful' });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error.code) {
+        switch (error.code) {
+          case 'permission-denied':
+            set({
+              categoriesStatus: 'rejected',
+              categoriesError: { error: error, errorMessage: 'Permission denied!' },
+            });
+            break;
+          case 'unauthenticated':
+            set({
+              categoriesStatus: 'rejected',
+              categoriesError: { error: error, errorMessage: 'Your not authenticated!' },
+            });
+            break;
+        }
+      } else {
         set({
-          categoriesLoading: false,
+          categoriesStatus: 'rejected',
           categoriesError: error,
         });
       }
-    } finally {
-      set({ categoriesLoading: false });
     }
   },
   deleteCategoryHandler: async (categoryId, isDeleteWithTasks, userId) => {
     try {
       set({
-        categoriesLoading: true,
+        categoriesStatus: 'pending',
         categoriesError: undefined,
       });
       if (isDeleteWithTasks) {
@@ -42,15 +75,30 @@ export const categoriesSlice: StateCreator<ICategoriesSlice, [], [], ICategories
       } else {
         await deleteCategory(categoryId);
       }
-    } catch (error) {
-      if (error instanceof Error) {
+      set({ categoriesStatus: 'successful' });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      if (error.code) {
+        switch (error.code) {
+          case 'permission-denied':
+            set({
+              categoriesStatus: 'rejected',
+              categoriesError: { error: error, errorMessage: 'Permission denied!' },
+            });
+            break;
+          case 'unauthenticated':
+            set({
+              categoriesStatus: 'rejected',
+              categoriesError: { error: error, errorMessage: 'Your not authenticated!' },
+            });
+            break;
+        }
+      } else {
         set({
-          categoriesLoading: false,
+          categoriesStatus: 'rejected',
           categoriesError: error,
         });
       }
-    } finally {
-      set({ categoriesLoading: false });
     }
   },
 });
